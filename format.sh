89c6e4e2b8f4e65e35e01f21661d9d6a1a1e8689
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2021-02-10 17:59:30 -0500

    .

--- format.sh
+++ format.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+function format {
+    which $1 2>&1 > /dev/null && $@
+}
 
-indent -o $1 $1
-clang-format --style=file -i $1
-astyle --options=astylerc --suffix=none $1 > /dev/null
-clang-format --style=file -i $1
+
+format indent -o $1 $1
+format clang-format --style=file -i $1
+format astyle --options=astylerc --suffix=none $1 > /dev/null
+format clang-format --style=file -i $1
+
